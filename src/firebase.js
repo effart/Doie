ad110d5b884f603b2f5f664691a6a1ef73ace02c
usama-jamil <usamajamil006@gmail.com>
2019-04-05 19:19:26 +0500

    Add Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { firebase } from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDHsbWyFmtIjoIeLpjInkUrvQhv4yYQ6JM",
+  authDomain: "react-redux-firebase-slack.firebaseapp.com",
+  databaseURL: "https://react-redux-firebase-slack.firebaseio.com",
+  projectId: "react-redux-firebase-slack",
+  storageBucket: "react-redux-firebase-slack.appspot.com",
+  messagingSenderId: "877635346986"
+};
+firebase.initializeApp(config);
+
+export default firebase;
