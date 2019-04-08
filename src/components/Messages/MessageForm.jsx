6d400ee3140ad87cfac3ee183f77b5893f43d13e
usama-jamil <usamajamil006@gmail.com>
2019-04-08 12:28:03 +0500

    28.Create Messages Collection,Add Message Database

--- src/components/Messages/MessageForm.jsx
+++ src/components/Messages/MessageForm.jsx
@@ -1,22 +1,94 @@
 import React, { Component } from 'react'
+import firebase from '../../firebase'
 import { Segment, Input, Button } from 'semantic-ui-react';
 
+
+
 export default class MessageForm extends Component {
+
+    state = {
+        message: '',
+        channel: this.props.currentChannel, //TODO: there is need to assign props to state
+        user: this.props.currentUser,       //TODO: there is need to assign props to state
+        loading:false,
+        errors:[],
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    createMessage =()=>{
+        const message={
+            timestamp: firebase.database.ServerValue.TIMESTAMP,
+            user:{
+                id:  this.state.user.uid,
+                name: this.state.user.displayName,
+                avatar: this.state.user.photoURL
+            },
+            content:this.state.message
+        }
+
+        return message;
+    }
+
+    sendMessage =()=>{
+        const{messagesRef}=this.props
+        const{message, channel}=this.state
+
+        if(message){
+            this.setState({loading:true})
+            messagesRef
+            .child(channel.id)
+            .push()
+            .set(this.createMessage())
+            .then(()=>{
+                this.setState({ loading: false, message:'', errors:[]})
+            })
+            .catch(err =>{
+                console.log(err)
+                this.setState({
+                    loading:false,
+                    errors: this.state.errors.concat(err)
+                })
+            })
+            
+        }else{
+            this.setState({errors: this.state.errors.concat({ message: "Add a message"})})
+        }
+    }
     render() {
+
+        const  {errors} =this.state
+
         return (
             <Segment className='message__form'>
                 <Input
                     fluid
                     name='message'
+                    onChange={this.handleChange}
                     style={{ marginBottom: '0.7em' }}
                     label={<Button icon='add' />}
                     labelPosition='left'
-                    placeholder='Write your message' />
+                    placeholder='Write your message'
+                    className={
+                        errors.some(error=>error.message.includes('message')) ? 'error':''
+                    } />
 
                 <Button.Group icon widths='2'>
-                <Button color='orange' content='Add Reply' labelPosition='left' icon='edit'/> 
-                <Button color='teal' content='Upload Media' labelPosition='right' icon='cloud upload'/> 
-                    </Button.Group>    
+                    <Button
+                        onClick={this.sendMessage}
+                        color='orange'
+                        content='Add Reply'
+                        labelPosition='left'
+                        icon='edit' />
+                    <Button
+                        color='teal'
+                        content='Upload Media'
+                        labelPosition='right'
+                        icon='cloud upload' />
+
+                </Button.Group>
             </Segment>
 
         )
--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import { Segment, Comment } from 'semantic-ui-react';
+import firebase from '../../firebase'
 
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 
 export default class Messages extends Component {
+
+  state ={
+    messagesRef: firebase.database().ref('messages'),
+    channel: this.props.currentChannel, //TODO: there is need to assign props to state
+    user: this.props.currentUser        //TODO: there is need to assign props to state
+  }
+
   render() {
+
+    const{messagesRef , channel ,user} =this.state
+
     return (
       <React.Fragment>
         <MessagesHeader/>
@@ -18,7 +29,11 @@ export default class Messages extends Component {
         </Segment>
 
 
-        <MessageForm/>
+        <MessageForm  
+        messagesRef={messagesRef}  
+        currentChannel={channel}
+        currentUser={user}
+        />
       </React.Fragment>
     )
   }
