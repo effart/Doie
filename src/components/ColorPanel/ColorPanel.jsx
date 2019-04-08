f9b3cccef63e409bddee18ddc701b277ac121828
usama-jamil <usamajamil006@gmail.com>
2019-04-08 10:22:23 +0500

    27. Scaffold ColorPanel  Message Components

--- src/components/ColorPanel/ColorPanel.jsx
+++ src/components/ColorPanel/ColorPanel.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { Sidebar, Menu, Divider, Button } from 'semantic-ui-react';
 
 export default class ColorPanel extends Component {
   render() {
     return (
-      <div>
-        ColorPanel
-      </div>
+      <Sidebar as={Menu} inverted vertical visible icon='labeled' width='very thin'>
+        <Divider />
+        <Button icon='add' size='small' color='blue' />
+      </Sidebar>
+
     )
   }
 }
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { Segment, Comment } from 'semantic-ui-react';
+
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
 
 export default class Message extends Component {
   render() {
     return (
-      <div>
-        Message
-      </div>
+      <React.Fragment>
+        <MessagesHeader/>
+
+        <Segment>
+        <Comment.Group className="messages">
+          {/* Messages */}
+        </Comment.Group>
+
+        </Segment>
+
+
+        <MessageForm/>
+      </React.Fragment>
     )
   }
 }
