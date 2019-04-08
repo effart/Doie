b1823d8c7376f8f8c26d520189f7cff1061f70b1
usama-jamil <usamajamil006@gmail.com>
2019-04-08 14:10:52 +0500

    30. Display Messages, Create Message Component

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -4,35 +4,68 @@ import firebase from '../../firebase'
 
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
+import Message from './Message';
 
 export default class Messages extends Component {
 
-  state ={
+  state = {
     messagesRef: firebase.database().ref('messages'),
     channel: this.props.currentChannel, //TODO: there is need to assign props to state
-    user: this.props.currentUser        //TODO: there is need to assign props to state
+    user: this.props.currentUser,        //TODO: there is need to assign props to state
+    messages: [],
+    messagesLoading:true,
   }
 
+  componentDidMount() {
+    const { user, channel } = this.state;
+
+    if (channel && user) {
+      this.addListeners(channel.id)
+    }
+  }
+
+  addListeners = channelId => {
+    this.addMessageListeners(channelId)
+  }
+
+  addMessageListeners = channelId => {
+    let loadedMessages = []
+
+    this.state.messagesRef.child(channelId).on('child_added', snap => {
+      loadedMessages.push(snap.val())
+
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false
+      })
+    })
+  }
+
+  displayMessages = messages=>(
+    messages.length > 0 &&  messages.map(message=>(
+      <Message key={message.timestamp} message={message} user={this.state.user}/>
+    ))
+  )
   render() {
 
-    const{messagesRef , channel ,user} =this.state
+    const { messagesRef,messages, channel, user } = this.state
 
     return (
       <React.Fragment>
-        <MessagesHeader/>
+        <MessagesHeader />
 
         <Segment>
-        <Comment.Group className="messages">
-          {/* Messages */}
-        </Comment.Group>
+          <Comment.Group className="messages">
+            {this.displayMessages(messages)}
+          </Comment.Group>
 
         </Segment>
 
 
-        <MessageForm  
-        messagesRef={messagesRef}  
-        currentChannel={channel}
-        currentUser={user}
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
         />
       </React.Fragment>
     )
