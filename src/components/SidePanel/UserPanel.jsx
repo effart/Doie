d20f31f6294f2a6177917d6d1d043a1e310216d8
usama-jamil <usamajamil006@gmail.com>
2019-04-21 19:11:36 +0500

    53. Create Change Avatar Modal

--- src/components/SidePanel/UserPanel.jsx
+++ src/components/SidePanel/UserPanel.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react"
+import { Grid, Header, Icon, Dropdown, Image, Modal, Input, Button } from "semantic-ui-react"
 import firebase from "../../firebase"
 
 
@@ -7,8 +7,12 @@ class UserPanel extends Component {
 
     // TODO: there is  no need of to assign props to state
     state = {
-        user: this.props.currentUser
+        user: this.props.currentUser,
+        modal:false
     }
+
+    openModal =()=>this.setState({modal:true})
+    closeModal =()=>this.setState({modal:false})
 
     dropdownOption = () => [
         {
@@ -18,7 +22,7 @@ class UserPanel extends Component {
         },
         {
             key: 'avatar',
-            text: <span>avatar</span>
+            text: <span onClick={this.openModal}>avatar</span>
         },
         {
             key: 'signout',
@@ -34,7 +38,7 @@ class UserPanel extends Component {
             .then((a) => console.log(a + 'signout'))
     }
     render() {
-        const { user } = this.state
+        const { user ,modal } = this.state
         const { primaryColor } =  this.props
         return (
             <Grid style={{ background: primaryColor }}>
@@ -45,7 +49,6 @@ class UserPanel extends Component {
                             <Icon name="code" />
                             <Header.Content>DevChat</Header.Content>
                         </Header>
-                    </Grid.Row>
 
                     {/* User DropDown */}
                     <Header inverted style={{ padding: "0.25em" }} as="h4" >
@@ -55,7 +58,41 @@ class UserPanel extends Component {
                         </span>}
                             options={this.dropdownOption()} />
                     </Header>
-
+                    </Grid.Row>
+                        
+                        {/* change User avatar Modal */}
+                        <Modal basic open={modal} onClose={this.closeModal}>
+                        <Modal.Header>Change Avatar</Modal.Header>
+                            <Modal.Content>
+                                <Input
+                                    fluid
+                                    type='file'
+                                    label='New Avatar'
+                                    name='previewImage'
+                                />
+                                <Grid centered stackable columns={2}>
+                                    <Grid.Row centered>
+                                        <Grid.Column className="ui center aligned grid">
+                                {/* Image Preview */}
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                            {/* Cropped image Preview */}
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button color='green' inverted>
+                                    <Icon name='save'/> Change Avatar
+                                </Button>
+                                <Button color='green' inverted>
+                                    <Icon name='image'/> Preview
+                                </Button>
+                                <Button color='red' inverted  onClick={this.closeModal}>
+                                    <Icon name='remove'/> Cancel
+                                </Button>
+                            </Modal.Actions>
+                        </Modal>
                 </Grid.Column>
             </Grid>
         )
