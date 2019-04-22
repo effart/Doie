4877072cfe03e1f8709b90498238e96c4cd8f2b4
usama-jamil <usamajamil006@gmail.com>
2019-04-22 13:36:01 +0500

    62.Create Key Combo Shortcuts to Send Messages

--- src/components/Messages/MessageForm.jsx
+++ src/components/Messages/MessageForm.jsx
@@ -34,7 +34,13 @@ export default class MessageForm extends Component {
         this.setState({ [event.target.name]: event.target.value })
     }
 
-    handleKeyDown =()=>{
+    handleKeyDown = event =>{
+        if(event.keyCode ===13){
+            this.sendMessage()
+        }
+        // if(event.ctrlKey && event.keyCode ===13){  //this combine ctrl + Enter
+        //     this.sendMessage()
+        // }
         const {message , typingRef ,channel ,user}= this.state
         if(message){
             typingRef
